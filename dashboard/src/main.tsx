57b5234e381d9f62b4d897f9922ead304231ccf7
Nicholas Suski <nick@suskitech.org>
2026-02-28 22:59:37 -0700

    Wrap App with ErrorBoundary at root level to prevent blank-page crashes

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ErrorBoundary } from './components/ErrorBoundary/ErrorBoundary.tsx'
 import { App } from './App.tsx'
 import './index.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary fallbackLabel="The dashboard crashed. Click 'Try Again' to reload.">
+      <App />
+    </ErrorBoundary>
   </StrictMode>,
 )
